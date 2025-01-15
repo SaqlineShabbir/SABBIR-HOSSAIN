374b76afc47685ca770f484af4839561a3cdee9d
SaqlineShabbir <saqline5869@gmail.com>
2025-01-15 07:43:06 -0800

    chnages

--- src/v1/components/Footer.jsx
+++ src/v1/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           Developed by Sabbir..
         </span>
         <span className="flex items-center gap-1">
-          <FaRegClock /> Updated: 14 Jan 2025
+          <FaRegClock /> Updated: 15 Jan 2025
         </span>
       </div>
     </div>
